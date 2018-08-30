dde4121af3288659bd54931deafb9a2785bc3b99
Arnaud <arnaud.voiron@gmail.com>
2018-08-30 11:55:21 +0200

    ajout classe reservation et reservation DAO

---
+++ VehicleRental/src/com/campusnumerique/vehiclerental/dao/ReservationDAO.java
@@ -0,0 +1,41 @@
+package com.campusnumerique.vehiclerental.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.campusnumerique.vehiclerental.entity.Client;
+import com.campusnumerique.vehiclerental.entity.Reservation;
+
+public class ReservationDAO  extends DAO<Reservation> {
+
+	@Override
+	public boolean create(Reservation obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Reservation obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Reservation obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Reservation find(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Reservation> findAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ VehicleRental/src/com/campusnumerique/vehiclerental/entity/Reservation.java
@@ -0,0 +1,74 @@
+package com.campusnumerique.vehiclerental.entity;
+
+import java.sql.Date;
+
+public class Reservation {
+
+	private int clientId;
+	private int carId;
+	private Date startDate;
+	private Date endDate;
+	private int estimatedKm;
+	private int realKm;
+	private int price;
+	
+	
+	public Reservation(int clientId, int carId, Date startDate, Date endDate, int estimatedKm, int realKm, int price) {
+		super();
+		this.clientId = clientId;
+		this.carId = carId;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.estimatedKm = estimatedKm;
+		this.realKm = realKm;
+		this.price = price;
+	}
+	
+	
+	public int getClientId() {
+		return clientId;
+	}
+	public void setClientId(int clientId) {
+		this.clientId = clientId;
+	}
+	public int getCarId() {
+		return carId;
+	}
+	public void setCarId(int carId) {
+		this.carId = carId;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public int getEstimatedKm() {
+		return estimatedKm;
+	}
+	public void setEstimatedKm(int estimatedKm) {
+		this.estimatedKm = estimatedKm;
+	}
+	public int getRealKm() {
+		return realKm;
+	}
+	public void setRealKm(int realKm) {
+		this.realKm = realKm;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+}
